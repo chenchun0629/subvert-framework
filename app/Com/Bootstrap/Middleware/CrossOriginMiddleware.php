6c11aad328cae474f92edf7b004646c08a8084a9
chenchun0629 <pangou@live.cn>
2016-09-15 00:52:06 +0800

    cross

--- app/Com/Bootstrap/Middleware/CrossOriginMiddleware.php
+++ app/Com/Bootstrap/Middleware/CrossOriginMiddleware.php
@@ -20,7 +20,7 @@ class CrossOriginMiddleware implements RequestMiddleware
             $response = app()->prepareResponse('');
 
             if (in_array($request->server('HTTP_ORIGIN'), config('cross_origin.allow_domain_list'))) {
-                $response->headers->set('Access-Control-Allow-Origin', config('cross_origin.doamin_schema') . $request->server('HTTP_ORIGIN'));
+                $response->headers->set('Access-Control-Allow-Origin',  $request->server('HTTP_ORIGIN'));
                 $response->headers->set('Access-Control-Allow-Methods', implode(', ', config('cross_origin.allow_request_type')));
                 $response->headers->set('Access-Control-Allow-Credentials', config('cross_origin.allow_credentials'));
                 $response->headers->set('Access-Control-Allow-Headers', implode(', ', config('cross_origin.allow_headers')));
@@ -33,7 +33,7 @@ class CrossOriginMiddleware implements RequestMiddleware
 
             $response = $next($request);
 
-            $response->headers->set('Access-Control-Allow-Origin', config('cross_origin.doamin_schema') . $request->server('HTTP_ORIGIN'));
+            $response->headers->set('Access-Control-Allow-Origin',  $request->server('HTTP_ORIGIN'));
             $response->headers->set('Access-Control-Allow-Methods', implode(', ', config('cross_origin.allow_request_type')));
             $response->headers->set('Access-Control-Allow-Credentials', config('cross_origin.allow_credentials'));
             $response->headers->set('Access-Control-Allow-Headers', implode(', ', config('cross_origin.allow_headers')));
